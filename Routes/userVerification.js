4cfdf2038869a2795f0e7f7cd8c9f9b6bd9bfc74
Anand <24manekara@rbunagpur.in>
2026-03-15 23:00:01 +0530

    Added MongoDB Atlas connection and signup/login API

--- Routes/userVerification.js
+++ Routes/userVerification.js
@@ -25,10 +25,9 @@ router.post("/signup", async (req, res) => {
 
     res.status(201).send("Signup successful");
   } catch (error) {
-    console.log(error);
-    res.status(500).send("Error while signing up");
-  }
-});
+  console.log("SIGNUP ERROR:", error);
+  res.status(500).send(error.message);
+}});
 
 router.post("/signin", async (req, res) => {
   try {
