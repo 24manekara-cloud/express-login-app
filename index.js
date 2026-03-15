bd7f74fe2c44ee7768cd805ffe8d931022e12011
Anand <24manekara@rbunagpur.in>
2026-03-15 21:16:32 +0530

    Initial project upload

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// const userLoginRouter = require("./Routes/userLogin");
-// const userVerificationRouter = require("./Routes/userVerification");
+const userLoginRouter = require("./Routes/userLogin");
+const userVerificationRouter = require("./Routes/userVerification");
 
 /* Middleware */
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* Routes */
-// app.use("/login", userLoginRouter);
-// app.use("/verify", userVerificationRouter);
+app.use("/login", userLoginRouter);
+app.use("/verify", userVerificationRouter);
 
 /* Home route */
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "Templates", "loginPage.html"));
-// });
 app.get("/", (req, res) => {
-    res.send("<h1>Server Working</h1>");
+    res.sendFile(path.join(__dirname, "Templates", "loginPage.html"));
 });
+
 /* Server */
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
